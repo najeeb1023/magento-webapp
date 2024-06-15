a26eb005cde59b24bf8f65c28326d3fce88214ac
najeeb1023 <najeeb.capricorn@gmail.com>
2024-06-16 00:12:18 +0200

    User registration if condition wip.

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { LaunchOptions } from "@playwright/test";
 
 export const config: LaunchOptions = {
   timeout: 600000,
-  headless: false,
+  headless: true,
   args:['--start-maximized'],
   slowMo: 300
   
